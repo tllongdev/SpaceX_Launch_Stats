9ba96566d5bda9b3a24ade9f30f7f2534c281271
Timothy Lee Long <tllong@MacBook-Pro.local>
2020-04-23 01:10:31 -0500

    Initial App Deployment

---
+++ public/precache-manifest.dc826e3f66b908babdd2340aeacd726e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d83ab211351ecd46c9b540e3d9249852",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6784c7e0add03f8e4de",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "d66e996799891fa13a26",
+    "url": "/static/js/2.b7f071b3.chunk.js"
+  },
+  {
+    "revision": "f0d43bb23094b147d2c500466b241ef2",
+    "url": "/static/js/2.b7f071b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6784c7e0add03f8e4de",
+    "url": "/static/js/main.dd204cec.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  },
+  {
+    "revision": "e6b35931408e57a2f5f4b548bb9d6673",
+    "url": "/static/media/logo.e6b35931.png"
+  }
+]);
