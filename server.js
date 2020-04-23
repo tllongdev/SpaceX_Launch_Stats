0ba52b0b068c8c05734163a35f3b09695b5100f0
Timothy Lee Long <tllong@MacBook-Pro.local>
2020-04-23 00:36:42 -0500

    Application Working on localhost

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
+const cors = require('cors')
 const schema = require('./schema')
 
 const app = express()
 
+// Allow cross-origin
+app.use(cors())
+
 app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema,
-    graphiql: true,
-  })
+	'/graphql',
+	graphqlHTTP({
+		schema,
+		graphiql: true,
+	})
 )
 
 const PORT = process.env.PORT || 5000
